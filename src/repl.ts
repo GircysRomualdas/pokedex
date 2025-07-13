0047e31ea3e13cb19177097a8a9e12f2c97f45e6
romas <r2udis@gmail.com>
2025-07-13 23:01:16 +0300

    add help and exit

--- src/repl.ts
+++ src/repl.ts
@@ -1,18 +1,37 @@
-import * as readline from "readline";
+import { createInterface } from "readline";
+
+import { getCommands } from "./command.js";
 
 export function startREPL() {
-  const readLine = readline.createInterface({
+  const readLine = createInterface({
     input: process.stdin,
     output: process.stdout,
     prompt: "Pokedex > ",
   });
 
-  readLine.prompt();
-  readLine.on("line", (line: string) => {
-    const cleanLines = cleanInput(line);
+  const commands = getCommands();
 
-    if (cleanLines.length > 0) {
-      console.log(`Your command was: ${cleanLines[0]}`);
+  readLine.prompt();
+  readLine.on("line", async (input) => {
+    const words = cleanInput(input);
+
+    if (words.length === 0) {
+      readLine.prompt();
+      return;
+    }
+
+    const commandName = words[0];
+    const command = commands[commandName];
+    if (!command) {
+      console.log("Unknown command");
+      readLine.prompt();
+      return;
+    }
+
+    try {
+      command.callback(commands);
+    } catch (err) {
+      console.log(err);
     }
 
     readLine.prompt();
