04f53b9adda31c11a8cf36fe56e095c9aaf01fd7
romas <r2udis@gmail.com>
2025-07-13 22:22:58 +0300

    add repl loop

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,24 @@
+import * as readline from "readline";
+
+export function startREPL() {
+  const readLine = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > ",
+  });
+
+  readLine.prompt();
+  readLine.on("line", (line: string) => {
+    const cleanLines = cleanInput(line);
+
+    if (cleanLines.length > 0) {
+      console.log(`Your command was: ${cleanLines[0]}`);
+    }
+
+    readLine.prompt();
+  });
+}
+
 export function cleanInput(input: string): string[] {
   return input
     .toLowerCase()
